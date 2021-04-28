942a28da96c56770b824b04c56f165901102cb38
rumi0608 <79518215+rumi0608@users.noreply.github.com>
2021-04-28 18:10:52 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,45 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+    
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+  attach(body){
+      this.sling.bodyA = body;
+
+}
+    fly(){
+        this.sling.null = null;
+    }
+
+    display(){
+         
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y);
+                 
+            }
+           
+            
+        }
+    }
+    
+}
+
